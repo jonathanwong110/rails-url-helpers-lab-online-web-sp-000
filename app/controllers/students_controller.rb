50ddf4ab5ec0d8fb97ed22d163069138f6552b37
Jonathan Wong <jonathan.wong110@gmail.com>
2019-08-30 04:12:16 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -10,13 +10,15 @@ class StudentsController < ApplicationController
   
   def activate
     @student = Student.find(params[:id])
-    
+
     if @student.active
-      @student.active = false
-    else
-      @student.active = true
-      @student.save
+       @student.active = false
+     else
+       @student.active = true
     end
+    @student.active
+    @student.save
+
     redirect_to student_path(@student)
   end
   
