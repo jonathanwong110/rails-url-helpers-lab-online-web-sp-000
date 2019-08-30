70e9e2d69b703e2e30ee7374313076f433c432e8
Jonathan Wong <jonathan.wong110@gmail.com>
2019-08-30 03:15:11 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -15,10 +15,8 @@ class StudentsController < ApplicationController
       @student.active = false
     else
       @student.active = true
+      @student.save
     end
-    @student.active
-    @student.save
-    redirect_to student_path(@student)
   end
 
   private
